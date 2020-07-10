61d10043e72eb75357a0cd8847574e33a83d0f79
jason <s1017349071@163.com>
2020-07-10 11:39:37 +0800

    Update index.js
    为vuex 增加logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createLogger from "vuex/dist/logger";
 import actions from "./action";
 import getters from "./getter";
 import mutations from "./mutations";
 import state from "./state";
 import shop from "./shop";
 Vue.use(Vuex);
-
+const debug = process.env.NODE_ENV !== "production";
 export default new Vuex.Store({
   state: { state },
   mutations: { mutations },
   actions: { actions },
   getters: { getters },
-  modules: { shop }
+  modules: { shop },
+  plugins: debug ? [createLogger()] : []
 });
